88384c2267c0acd755e1f114b7f4a3d4b728774e
shahzaintariq <shahzaintariq.crpyto@gmail.com>
2024-12-08 20:23:33 +0500

    project 1

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,17 @@
 import type { Config } from "tailwindcss";
 
-export default {
+const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      fontFamily: {
+        IntegralCF: ['Integral CF', 'sans-serif'],
+        Satoshi: ['Satoshi', 'sans-serif'],
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
@@ -15,4 +19,5 @@ export default {
     },
   },
   plugins: [],
-} satisfies Config;
+};
+export default config;
